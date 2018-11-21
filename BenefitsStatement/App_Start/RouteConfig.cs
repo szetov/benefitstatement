055c7912846d6a8067f40ae049cdde4d66e0838c
szetov <szetov@gmail.com>
2018-11-21 23:09:13 +0800

    disable mobile redirect to take advantage of Bootstrap's responsiveness

--- BenefitsStatement/App_Start/RouteConfig.cs
+++ BenefitsStatement/App_Start/RouteConfig.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Web;
 using System.Web.Routing;
+using System.Web.UI;
 using Microsoft.AspNet.FriendlyUrls;
+using Microsoft.AspNet.FriendlyUrls.Resolvers;
 
 namespace BenefitStatement
 {
@@ -12,7 +14,25 @@ namespace BenefitStatement
         {
             var settings = new FriendlyUrlSettings();
             settings.AutoRedirectMode = RedirectMode.Permanent;
-            routes.EnableFriendlyUrls(settings);
+            routes.EnableFriendlyUrls(settings, new NoMobileFriendlyUrlResolver());
+        }
+    }
+
+    /// <summary>
+    /// Disable mobile redirect to take advantage of Bootstrap's responsive design
+    /// </summary>
+    public class NoMobileFriendlyUrlResolver : WebFormsFriendlyUrlResolver
+    {
+        protected override bool TrySetMobileMasterPage(HttpContextBase httpContext, Page page, String mobileSuffix)
+        {
+            if (mobileSuffix == "Mobile")
+            {
+                return false;
+            }
+            else
+            {
+                return base.TrySetMobileMasterPage(httpContext, page, mobileSuffix);
+            }
         }
     }
 }
